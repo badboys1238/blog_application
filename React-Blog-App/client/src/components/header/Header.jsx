94e6701ec985020834f6a70a96e50cf542a5371c
badboys1238 <saadmalik12349@gmail.com>
2022-04-16 12:36:25 +0600

    all changes done

---
+++ React-Blog-App/client/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">LATEST NEWS</span>
+        <span className="headerTitleLg">Explore Tech World</span>
+      </div>
+    </div>
+  );
+}
---
+++ React-Blog-App/client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT US</span>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7MCeQ-Ia6oosUFvrVOBlm1-MWPzLXIBgGLQ&usqp=CAU"
+          alt=""
+        />
+        <p>
+          At this age of modern technology where new gadgets and digital apps are developed or created each day, tech websites and blogs come in handy.Internet users would know these new technologies through reading blogs.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+              <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US ON</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
